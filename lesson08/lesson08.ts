fdde68c377ed54bf1c385c8104a8d37dff0c0a74
apple <apple@appledeMacBook-Pro.local>
2019-05-07 20:42:42 +0800

    day5

--- lesson08/lesson08.ts
+++ lesson08/lesson08.ts
@@ -81,11 +81,11 @@ function f(x: demoEnum5) {
 
 
 // 外部枚举
-// 外部枚举用来描述已经存在的枚举类型的形状。
-
-declare enum demoEnum6 {
+// 外部枚举用来描述已经存在的枚举类型的形状。(看不懂。。。。。)
+declare enum demoEnum5 {
     A = 1,
     B,
     C = 2
 }
+console.log('declear demoEnum5 : '+JSON.stringify(demoEnum5));
 // 外部枚举和非外部枚举之间有一个重要的区别，在正常的枚举里，没有初始化方法的成员被当成常数成员。 对于非常数的外部枚举而言，没有初始化方法时被当做需要经过计算的。
